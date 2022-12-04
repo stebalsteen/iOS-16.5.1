e3b06d8820e9d6cef1957b996f2de208bb55bfc3
Serg <ssstebakov@gmail.com>
2022-12-04 12:09:47 +0300

    new commit

---
+++ 16.5.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  16.5.1
+//
+//  Created by Sergei Stebakov on 03.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
